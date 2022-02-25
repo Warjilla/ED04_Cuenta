0e16befa5244b0c0543618df9c16476af6664276
Warjilla <alvaro1982@gmail.com>
2022-02-25 21:08:43 +0100

    refactor paquete cuentas, abstacion operativa cuenta

--- src/main/java/cuentas/Main.java
+++ src/main/java/cuentas/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.ccuenta_ed04;
+package cuentas;
 
 /**
  *
@@ -11,18 +11,20 @@ package com.mycompany.ccuenta_ed04;
 public class Main {
 
     public static void main(String[] args) {
-        operativa_cuenta(2500);               
+        operativa_cuenta("Antonio López", "1000-2365-85-1230456789", 2500);               
     }
     
     /**
     * Esta funcion gestiona la operativa de una cuenta corriente.
     * @param cantidad. cantidad inicial de la cuenta.
+    * @param nombre_cuenta
+    * @param numero_cuenta
     */
-    public static void operativa_cuenta(float cantidad){
+    public static void operativa_cuenta( String nombre_cuenta, String numero_cuenta, float cantidad){
         CCuenta cuenta1;
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        cuenta1 = new CCuenta(nombre_cuenta, numero_cuenta,2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
